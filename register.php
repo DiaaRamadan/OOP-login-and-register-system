7859c75d3a7531ea0c6c6c64c92fbcc8bbb4235b
Diaa <deia.atiya100@gmail.com>
2019-09-07 01:07:42 +0200

    add register form

---
+++ register.php
@@ -0,0 +1,23 @@
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" autocomplete="off">
+    </div>
+
+    <div class="field">
+        <label for="password">Choose a password</label>
+        <input type="password" name="password" id="password">
+    </div>
+
+    <div class="field">
+        <label for="password_again">Enter password again</label>
+        <input type="password" name="password_again" id="password_again">
+    </div>
+
+    <div class="field">
+        <label for="name">Enter your name </label>
+        <input type="text" name="name" id="name">
+    </div>
+
+    <input type="submit" value="Register">
+</form>
