65a71cdcefdc8145ddd8ff50c75aa8ced8dacba9
Diaa <deia.atiya100@gmail.com>
2019-09-06 21:19:03 +0200

    finish query function and action

--- classes/DB.php
+++ classes/DB.php
@@ -6,7 +6,7 @@ class DB{
             $_query,
             $_error =false,
             $_results,
-            $count = 0;
+            $_count = 0;
 
     private function __construct()
     {
@@ -26,5 +26,70 @@ class DB{
              self::$_instance = new DB();
         }
         return self::$_instance;
+    }
+
+    public function query($sql, $params = array('diaa')){
+        $this->_error = false;
+        if($this->_query = $this->_Pdo->prepare($sql)){
+            if(count($params)){
+                $x = 1;
+                foreach ($params as $param){
+
+                    $this->_query->bindValue($x, $param);
+                    $x++;
+                }
+            }
+            if($this->_query->execute()){
+
+                $this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
+                $this->_count = $this->_query->rowCount();
+
+            }else{
+
+                $this->_error = true;
+            }
+        }
+        return $this;
+    }
+
+    public function action($action, $table, $where = array()){
+
+        if(count($where)){
+            $field = $where[0];
+            $operator = $where[1];
+            $value = $where[2];
+
+            $operators = array('=', '<', '>', ',<=', '>=');
+            if(in_array($operator, $operators)){
+                $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
+
+                if(!$this->query($sql, array($value))->error()){
+
+                    return $this;
+                }
+            }
+
+        }
+        return false;
+    }
+    public function get($table, $where){
+        return $this->action("SELECT * ", $table, $where);
+    }
+
+    public function delete($table, $where){
+        return $this->action("delete", $table, $where);
+    }
+
+    public function error(){
+
+        return $this->_error;
+    }
+
+    public function count(){
+
+        return $this->_count;
+    }
+    public function results(){
+        return $this->_results;
     }
 }
--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 require "core/init.php";
 
- DB::getInstance();
+$user = DB::getInstance()->get('users', array('username','=', 'diaa'));
+
+if(!$user->count()){
+
+    echo 'error';
+}else{
+
+   foreach ($user->results() as $result){
+
+       echo $result->username;
+   }
+}
+
